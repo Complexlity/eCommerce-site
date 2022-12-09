b0f0f1f3b298e101a67d28e280299fd11d00ce0b
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-09 15:40:39 +0100

    Add redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from "./store/counterSlice"
+import type { RootState } from './store/index.js'
+
 
 function App() {
+  const count = useSelector((state: RootState) => state.counter.value )
+  const dispatch = useDispatch()
   return (
-    <div className="App">
+    <div className="App h-screen bg-orange-500 mx-auto text-center text-5xl">
+      <Navbar />
+      <Home />
+      <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <div className="mt-10">{count}</div>
     </div>
   )
 }
---
+++ src/components/Shop.tsx
@@ -0,0 +1,7 @@
+const Shop = () => {
+    return ( 
+        <div>This is the shop</div>
+     );
+}
+ 
+export default Shop;

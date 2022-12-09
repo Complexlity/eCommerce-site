800e5c186b7da8b329af6f15526b8c1ff80bb529
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-10 00:22:24 +0100

    Use redux to increment cart count

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,8 @@
 import { FC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "../store/index.js";
+import { increment, decrement } from "../store/counterSlice";
+
 interface Props {
   color: string;
   rounding: string;
@@ -6,6 +10,9 @@ interface Props {
 }
 
 const Button: FC<Props> = ({ color, rounding, width }) => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
   const style: any = {
     backgroundColor: color,
     borderRadius: rounding,
@@ -15,6 +22,7 @@ const Button: FC<Props> = ({ color, rounding, width }) => {
     <button
       style={style}
       className="py-2  text-center hover:text-white hover:opacity-[80%]"
+      onClick={() => dispatch(increment())}
     >
       Add To Cart
     </button>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import type { RootState } from "../store/index.js";
+import { useDispatch, useSelector } from "react-redux";
 
 const Navbar = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
   return (
     <ul className="mx-auto mb-2 flex w-4/5 items-center justify-end bg-green-300 px-4 text-2xl">
       <li className="h-full bg-green-700 py-2 px-2 hover:bg-green-400 hover:text-gray-200">
@@ -16,10 +19,10 @@ const Navbar = () => {
         </Link>
       </li>
       <li className="ml-2 h-full">
-        <div className="relative bg-blue-500  hover:text-white">
+        <div className="relative grid  items-center justify-center rounded-full bg-blue-500 p-1 hover:scale-[110%]">
           <AiOutlineShoppingCart cursor={"pointer"} height="100%" />
           <span className="absolute bottom-[-.3rem] left-4 flex h-[1rem] w-[1rem]  items-center justify-center rounded-full bg-red-500 text-xs">
-            {12}
+            {count}
           </span>
         </div>
       </li>

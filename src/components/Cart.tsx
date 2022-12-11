46e1e9bdc97b7cf86f69bc883b6bf5e5cd6972f2
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-11 23:05:04 +0100

    Revert "Change overlay behaviour"
    This reverts commit f8fca74cf9abf28bebc3a5f2230d3a41ef8f97a2.

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -21,7 +21,6 @@ const Cart: FC<Props> = () => {
   body.style.overflowX = "hidden";
   body.style.overflowY = overlay ? "hidden" : "auto";
   const divStyle = overlay ? "open" : "closed";
-  const opacityStyle = overlay ? "opacity-[50%]" : "opacity-0";
 
   function alertUser(): void {
     return totalPrice > 0
@@ -34,13 +33,14 @@ const Cart: FC<Props> = () => {
   }
   return (
     <>
-      <div
-        className={
-          opacityStyle +
-          " min-h- absolute inset-0 w-full bg-gray-800 opacity-[50%]"
-        }
-        onClick={closeCart}
-      ></div>
+      {overlay && (
+        <div className="z-2 fixed inset-0  min-h-screen">
+          <div
+            className=" min-h- absolute inset-0 w-full bg-gray-800 opacity-[50%]"
+            onClick={closeCart}
+          ></div>
+        </div>
+      )}
       <div
         className={
           divStyle +

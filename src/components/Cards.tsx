ac1128e1779f79ca95eafc3ac874acb1dbda48c0
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-11 20:04:11 +0100

    Add cards responsiveness for extra-small devices

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -8,7 +8,7 @@ interface Props {}
 const Cards: FC<Props> = () => {
   const allItems = useSelector((state: RootState) => state.itemsList);
   return (
-    <div className="my-grid md:w/4/5 mx-auto grid max-w-[1100px] grid-cols-2 gap-4 px-4 md:grid-cols-3 lg:grid-cols-4 xl:gap-6">
+    <div className="my-grid md:w/4/5 mx-auto grid max-w-[1100px] gap-4 px-4 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-6">
       {allItems.map((item) => (
         <Card
           key={item.id}
